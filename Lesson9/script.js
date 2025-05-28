44590a47bcb7b2cf7a1b636d75f3b68f4e62cbe3
Ntombifuthi Mashinini <ntombifuthimashinini4@gmail.com>
2025-05-28 14:12:30 +0200

    Show scoped variables in daily routine

---
+++ Lesson9/script.js
@@ -0,0 +1,17 @@
+let location = "Rosebank";
+
+    function startMorning() {
+      let name = "Ntombi";
+      console.log(name + " woke up in " + location + ".");
+
+      let weather = "sunny";
+
+      if (weather === "sunny") {
+        let mood = "happy";
+        console.log(name + " feels " + mood + " because it's " + weather + ".");
+      }
+
+    }
+
+    console.log("The day starts in " + location + ".");
+
